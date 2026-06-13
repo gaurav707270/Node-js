4ce389aebbe76a160e8a901f5aff8b948592456f
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-06-13 13:52:41 +0530

    first commit

---
+++ day01/cartModul.js
@@ -0,0 +1,10 @@
+const addcart = () => {
+    return "add to cart"
+}
+
+const changeQty = () => {
+    return 5
+}
+
+// module.exports = addcart
+module.exports = { addcart, changeQty }
---
+++ day01/index.js
@@ -0,0 +1,8 @@
+const { addcart, changeQty } = require("./cartModul")
+
+console.log("wellcome")
+
+console.log(45 * 4)
+
+console.log(addcart())
+console.log(changeQty())
