91ff26f4bbc2bc4ab44a582f6ff03bcaca710e5d
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-06-16 12:22:53 +0530

    16/06/2026

--- express-project/index.js
+++ express-project/index.js
@@ -6,4 +6,8 @@ app.get("/", (req, res) => {
     res.send({ status: 200, msg: "home page Api" })
 })
 
+app.get("/news",(req,res) =>{
+    res.send({news: "student news", student  : 50 ,})
+})
+
 app.listen("5000")
