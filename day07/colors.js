645756d914bba8df1b00a68341b294c81e31816f
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-06-25 12:59:45 +0530

    25/06/2026

---
+++ day07/colors.js
@@ -0,0 +1,5 @@
+const colors = require("colors")
+
+console.log(colors.red("hi this side kharate gaurav"))
+console.log(colors.green("hi this side kharate gaurav"))
+console.log(colors.blue("hi this side kharate gaurav"))
