98bb0f5663f4ee4c374f97fb4ef39c0f00190952
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-08-10 10:18:00 +0530

    Update Server.js

--- day04/Server.js
+++ day04/Server.js
@@ -12,7 +12,7 @@ if (!fs.existsSync("logs.txt")) {
 
     const data = Date.now();
 
-    res.writeHead(200, {
+    // res.writeHead(200, {
         // "Contect-Type": "text/plain"
     })
     // res.end("Server Running");
