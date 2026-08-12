e506e8968fa75aa2fc874aadb4d82247b5f8b830
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-08-12 22:25:23 +0530

    Uncomment multiplication console log

--- day01/index.js
+++ day01/index.js
@@ -2,7 +2,7 @@ const { addcart, changeQty } = require("./cartModul")
 // 
 // console.log("wellcome")
 // 
-// console.log(45 * 4)
+console.log(45 * 4)
 // 
 // console.log(addcart())
 // console.log(changeQty())
