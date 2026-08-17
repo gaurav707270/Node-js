f665980a68945040c2bad7582660214a696cd9a0
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-08-17 16:34:05 +0530

    Uncomment server creation in index.js

--- day02/index.js
+++ day02/index.js
@@ -1,6 +1,6 @@
 const http = require("http")
 
-// const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     if (req.url == "/") {
         // res.end("welcome to http server")
     }
