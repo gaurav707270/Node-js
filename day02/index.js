399a7cf82e3f21622871808e55c333303d83132e
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-08-17 16:34:12 +0530

    Uncomment welcome message in HTTP server

--- day02/index.js
+++ day02/index.js
@@ -2,7 +2,7 @@ const http = require("http")
 
 const server = http.createServer((req, res) => {
     if (req.url == "/") {
-        // res.end("welcome to http server")
+        res.end("welcome to http server")
     }
     // if (req.url == "/news") {
         // const news = {
