37725defb7e0d7d4d003f9ee063d9f454bb2e134
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-09-02 10:54:21 +0530

    Update index.js

--- express-project/index.js
+++ express-project/index.js
@@ -5,7 +5,7 @@ const app = express()
 app.get("/", (req, res) => {
     res.send({ status: 200, msg: "home page Api" })
 })
-// 
+
 app.get("/news", (req, res) => {
     // res.send({ news: "student news", student: 50, })
 })
