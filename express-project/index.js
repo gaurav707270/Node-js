6b41436df790220ad8fc69eae8f1fc3d8690e462
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-09-02 10:53:47 +0530

    Update index.js

--- express-project/index.js
+++ express-project/index.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 })
 
 // app.post("/login", (req, res) => {
-    console.log(req.body)
+    // console.log(req.body)
     res.send({ news: "student news", student: 50, data: req.body })
 })
 
